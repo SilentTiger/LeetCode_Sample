7a426a4919819169ca77777199e63107f689177c
SilentTiger <cia.fbi.007@hotmail.com>
2014-11-11 18:18:29 +0800

    add start log

--- src/LeetCode/Main.java
+++ src/LeetCode/Main.java
@@ -4,6 +4,7 @@ import java.util.Hashtable;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.print("app start!\r\n");
 	}
 	
 	private static Hashtable<String, Long> timerData = new Hashtable<String, Long>();
