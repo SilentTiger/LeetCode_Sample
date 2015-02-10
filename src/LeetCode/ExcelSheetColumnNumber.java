e5f45c4e4d5e7f65d4a87954fe5a81f512b30d08
SilentTiger <cia.fbi.007@hotmail.com>
2015-02-10 18:04:42 +0800

    Revert "Revert "add ExcelSheetColumnNumber""
    This reverts commit 1cc202b03c02b521c1a6f388c9b988425b38e2d3.

---
+++ src/LeetCode/ExcelSheetColumnNumber.java
@@ -0,0 +1,71 @@
+package LeetCode;
+
+public class ExcelSheetColumnNumber {
+	public int titleToNumber(String s) {
+		int res = 0;
+		int stringLength = s.length();
+		for (int i = stringLength - 1; i >= 0; i--) {
+			res += ((s.charAt(i) - '@') * Math.pow(26, (stringLength - i - 1)));
+		}
+		return res;
+	}
+
+	public byte toByte(Character n) {
+		switch (n) {
+		case 'A':
+			return 1;
+		case 'B':
+			return 2;
+		case 'C':
+			return 3;
+		case 'D':
+			return 4;
+		case 'E':
+			return 5;
+		case 'F':
+			return 6;
+		case 'G':
+			return 7;
+		case 'H':
+			return 8;
+		case 'I':
+			return 9;
+		case 'J':
+			return 10;
+		case 'K':
+			return 11;
+		case 'L':
+			return 12;
+		case 'M':
+			return 13;
+		case 'N':
+			return 14;
+		case 'O':
+			return 15;
+		case 'P':
+			return 16;
+		case 'Q':
+			return 17;
+		case 'R':
+			return 18;
+		case 'S':
+			return 19;
+		case 'T':
+			return 20;
+		case 'U':
+			return 21;
+		case 'V':
+			return 22;
+		case 'W':
+			return 23;
+		case 'X':
+			return 24;
+		case 'Y':
+			return 25;
+		case 'Z':
+			return 26;
+		default:
+			return 0;
+		}
+	}
+}
